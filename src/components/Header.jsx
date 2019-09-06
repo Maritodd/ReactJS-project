cb21985186be587b468b1c09f76913eeb9e16f0f
yaroslav <yaroslav9190@gmail.com>
2019-09-06 22:59:58 +0300

    1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <img src='.\logo192.png' alt='logo' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <main className='content'>
+            <img src='https://www.optomaeurope.com/images/ProductApplicationFeatures/4kuhd/banner.jpg' />
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Profile;
